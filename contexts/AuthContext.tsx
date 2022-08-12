8acec5bf1b00331d977f6549528355f9791ac05a
Eduardo Andrade <edu2andrade@gmail.com>
2022-08-12 10:37:41 +0100

    Authentication with JWT implemented

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,103 @@
+import Router from "next/router";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
+
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token');
+  destroyCookie(undefined, 'nextauth.refreshToken');
+
+  Router.push('/');
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !! user;
+
+  useEffect(()=> {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if(token) {
+      api.get('/me')
+        .then(response => {
+          const { email, permissions, roles } = response.data;
+
+          setUser({ email, permissions, roles })
+        })
+        .catch(() => {
+          signOut();
+        })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials) {
+
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      })
+
+      const { permissions, roles, token, refreshToken } = response.data
+
+      // localStorage x
+      // SessionStorage x
+      // cookies (yes!) nookies library!
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      });
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      });
+
+      
+  
+      setUser({
+        email,
+        permissions, 
+        roles
+      }) 
+      // Send token since first login
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      Router.push('/dashboard');
+
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
